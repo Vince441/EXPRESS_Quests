3b78e65a9e2e818519381f1a34c8786b388101e8
Vince441 <vincentlouvart82@gmail.com>
2023-11-27 22:18:21 +0100

    ajout quest

---
+++ src/controllers/movieControllers.js
@@ -0,0 +1,63 @@
+// const movies = [
+//   {
+//     id: 1,
+//     title: "Citizen Kane",
+//     director: "Orson Wells",
+//     year: "1941",
+//     color: false,
+//     duration: 120,
+//   },
+//   {
+//     id: 2,
+//     title: "The Godfather",
+//     director: "Francis Ford Coppola",
+//     year: "1972",
+//     color: true,
+//     duration: 180,
+//   },
+//   {
+//     id: 3,
+//     title: "Pulp Fiction",
+//     director: "Quentin Tarantino",
+//     year: "1994",
+//     color: true,
+//     duration: 180,
+//   },
+// ];
+
+const database = require("../../database");
+
+const getMovies = (req, res) => {
+  database
+    .query("select * from movies")
+    .then(([movies]) => {
+      res.json(movies); // use res.json instead of console.log
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getMovieById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([movies]) => {
+      if (movies.length !== 0) {
+        res.json(movies);
+      } else {
+        res.status(404).send("Not Found");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  getMovies,
+  getMovieById,
+};
