9d2ac3e186bd76dad06e6a2e5136c96eeb9d40ba
Vince441 <vincentlouvart82@gmail.com>
2023-11-28 22:41:54 +0100

    Methode PUT

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -30,7 +30,48 @@ const getUsersById = (req, res) => {
     });
 };
 
+const postUsers = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  database
+    .query(
+      "INSERT INTO users(lastname, firstname, email, city, language) VALUES (?,?,?,?,?)",
+      [lastname, firstname, email, city, language]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const updateUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
 module.exports = {
-    getUsers,
-    getUsersById,
+  getUsers,
+  getUsersById,
+  postUsers,
+  updateUsers
 };
