32eb016db8516c2b36eccd2ac536dac4fbacff05
Vince441 <vincentlouvart82@gmail.com>
2023-11-29 20:14:24 +0100

    correction

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -69,9 +69,29 @@ const updateUsers = (req, res) => {
 };
 
 
+const deleteUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+
 module.exports = {
   getUsers,
   getUsersById,
   postUsers,
-  updateUsers
+  updateUsers,
+  deleteUsers,
 };
